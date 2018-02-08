67cb486231486d86a982ee96a48b334af6a36807
Stéphane Brunner <stephane.brunner@camptocamp.com>
2018-02-08 16:08:18 +0100

    Revert "try"
    This reverts commit 1ef87b6e73e6ebf9bc473affabb090b4872be396.

--- test/spec/services/printutils.spec.js
+++ test/spec/services/printutils.spec.js
@@ -3,26 +3,32 @@ goog.require('ngeo.print.Utils');
 describe('ngeo.print.Utils', () => {
 
   let ngeoPrintUtilsService;
-  let inchesPerMeter, dotsPerInch;
 
   beforeEach(() => {
     angular.mock.inject((ngeoPrintUtils) => {
       ngeoPrintUtilsService = ngeoPrintUtils;
     });
-    inchesPerMeter = ngeo.print.Utils.INCHES_PER_METER_;
-    dotsPerInch = ngeo.print.Utils.DOTS_PER_INCH_;
-  });
-
-  afterEach(() => {
-    ngeo.print.Utils.INCHES_PER_METER_ = inchesPerMeter;
-    ngeo.print.Utils.DOTS_PER_INCH_ = dotsPerInch;
   });
 
   describe('#getOptimalResolution', () => {
-    it('returns the optimal resolution', () => {
+
+    let inchesPerMeter, dotsPerInch;
+
+    beforeEach(() => {
+      inchesPerMeter = ngeo.print.Utils.INCHES_PER_METER_;
+      dotsPerInch = ngeo.print.Utils.DOTS_PER_INCH_;
+
       // consider 3200 dots per meter
       ngeo.print.Utils.INCHES_PER_METER_ = 40;
       ngeo.print.Utils.DOTS_PER_INCH_ = 80;
+    });
+
+    afterEach(() => {
+      ngeo.print.Utils.INCHES_PER_METER_ = inchesPerMeter;
+      ngeo.print.Utils.DOTS_PER_INCH_ = dotsPerInch;
+    });
+
+    it('returns the optimal resolution', () => {
       const mapSize = [2, 1];  // px
       const printMapSize = [640, 320];  // dots
       const printScale = 10;  // scale denominator
